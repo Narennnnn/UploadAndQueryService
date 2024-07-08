42e14967d899bd95ff5da906145c17be9bc8bb3b
Narendra Maurya <mauryanarendra2003@gmail.com>
2024-07-08 11:40:56 +0530

    feat: docker setup

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from app.validusers import add_sample_users
 
 if __name__ == '__main__':
     app = create_app()
+    app.run(host='0.0.0.0')
     # with app.app_context():
     #     add_sample_users()
-    app.run(debug=True)
+    app.run()
+
+
